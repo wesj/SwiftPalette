4603b1b34e1a38a8b0503466c19063c51f187a06
Wes Johnston <wjohnston@mozilla.com>
2015-06-20 19:00:10 -0700

    Initial Commit

---
+++ Palette3/Palette3.h
@@ -0,0 +1,19 @@
+//
+//  Palette3.h
+//  Palette3
+//
+//  Created by Wes Johnston on 6/20/15.
+//  Copyright (c) 2015 Wes Johnston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Palette3.
+FOUNDATION_EXPORT double Palette3VersionNumber;
+
+//! Project version string for Palette3.
+FOUNDATION_EXPORT const unsigned char Palette3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Palette3/PublicHeader.h>
+
+
